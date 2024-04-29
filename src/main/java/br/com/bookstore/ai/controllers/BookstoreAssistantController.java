b2d18ae03c38a231b91a01d2d98d9752cda5ca1f
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2024-04-28 21:26:40 -0300

    feat: using prompt-template to generate responses

--- src/main/java/br/com/bookstore/ai/controllers/BookstoreAssistantController.java
+++ src/main/java/br/com/bookstore/ai/controllers/BookstoreAssistantController.java
@@ -2,6 +2,7 @@ package br.com.bookstore.ai.controllers;
 
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,10 +27,22 @@ public class BookstoreAssistantController {
 
     // }
 
-    @GetMapping("/informations")
-    public ChatResponse bookstoreChatEx2(
-            @RequestParam(value = "message", defaultValue = "Quais são os livros best sellers dos ultimos anos?") String message) {
-        return openAiChatClient.call(new Prompt(message));
+    // @GetMapping("/informations")
+    // public ChatResponse bookstoreChatEx2(
+    // @RequestParam(value = "message", defaultValue = "Quais são os livros best
+    // sellers dos ultimos anos?") String message) {
+    // return openAiChatClient.call(new Prompt(message));
+
+    // }
+
+    @GetMapping("/reviews")
+    public String bookstoreReview(@RequestParam(value = "book", defaultValue = "Dom Quixote") String book) {
+        PromptTemplate promptTemplate = new PromptTemplate("""
+                Por favor, me forneça um breve resumo
+                do livro {book} e também a biografia do
+                seu autor.""");
+        promptTemplate.add("book", book);
+        return this.openAiChatClient.call(promptTemplate.create()).getResult().getOutput().getContent();
 
     }
 
